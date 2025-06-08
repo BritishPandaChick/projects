16dfcad9d9a38bf77a5556559be56dd870840ccd
britishpandachick <sbartley1080@gmail.com>
2025-06-07 20:08:32 -0400

    add bulma to head tag. Start updating cards code. Add JS code for navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
-// add class navbarDark on navbar scroll 
-const header = document.querySelector('.navbar');
+document.addEventListener('DOMContentLoaded', () => {
 
-window.onscroll = function() {
-  var top = window.scrollY;
-  if(top >= 100) {
-    header.classList.add('navbarDark');
-  } else {
-    header.classList.remove('navbarDark');
-  }
-}
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+});
